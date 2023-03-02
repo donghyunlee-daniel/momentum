ec145c86e5f486fe79631428fbbb0188324c3bc9
d.lee77668@gmail.com <d.lee77668@gmail.com>
2023-03-02 18:29:45 -0500

    Finished sign in part

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+const signInForm = document.querySelector("#login-form");
 const signInInput = document.querySelector("#login-form input");
-const signInButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function handleSignInBtnClick(){
-    console.log(signInInput.value);
- 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+function onSignInSubmit(event){
+    event.preventDefault();
+    signInForm.classList.add(HIDDEN_CLASSNAME);
+    const username = signInInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
-signInButton.addEventListener("click",handleSignInBtnClick);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null)
+{
+    signInForm.classList.remove(HIDDEN_CLASSNAME);
+    signInForm.addEventListener("submit", onSignInSubmit); 
+}
+else{
+    paintGreetings(savedUsername);
+}
